9002937b12dde8f11aca67b215a29de3735b157a
Amulya Metku <devi.amulya72@gmail.com>
2018-01-16 20:23:21 +0530

    Initial Commit

---
+++ HangMan.py
@@ -0,0 +1,10 @@
+
+import random
+
+setOfWords = '''apple banana mango strawberry 
+orange grape pineapple apricot lemon coconut watermelon
+cherry papaya berry peach lychee muskmelon'''
+
+Words_Available = setOfWords.split(' ')
+my_word = random.choice(Words_Available)
+
